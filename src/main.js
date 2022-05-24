9138818e427f51a2a6550cac82714fb1ed0d5d3d
lizatalkina <liza.talkina@gmail.com>
2022-05-24 14:04:57 +0300

    Реализована кнопка Show more, сделана заглушка

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import ProfileView from './view/profile-view.js';
 import FilterPresenter from './presenter/filter-presenter.js';
-import SortView from './view/sort-view.js';
 import {render} from './render.js';
 import ListPresenter from './presenter/list-presenter.js';
 import MoviesModel from './model/movie-model.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
-const listPresenter = new ListPresenter();
-const filterPresenter = new FilterPresenter();
 const moviesModel = new MoviesModel();
+const listPresenter = new ListPresenter(siteMainElement, moviesModel);
+const filterPresenter = new FilterPresenter();
+
 
 render(new ProfileView(), siteHeaderElement);
 filterPresenter.init(siteMainElement, moviesModel);
-render(new SortView(), siteMainElement);
 
-listPresenter.init(siteMainElement, moviesModel);
+listPresenter.init();
